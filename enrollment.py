f3c9ef89da55d4339527a8a40fb8067333d73e8c
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-02 10:30:58 +0100

    Create enrollment.py

---
+++ enrollment.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.markdown('<div class="title">Enrollment</div>', unsafe_allow_html=True)
+    st.markdown('<div class="content">Enrollment details and registration information will be available soon. Stay tuned for updates on how to join the course!</div>', unsafe_allow_html=True)
