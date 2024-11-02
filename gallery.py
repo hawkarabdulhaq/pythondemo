0b3817d0d5aca5d94a1e0623e1bd67bbc4f83384
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-02 10:30:11 +0100

    Create gallery.py

---
+++ gallery.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.markdown('<div class="title">Gallery</div>', unsafe_allow_html=True)
+    st.markdown('<div class="content">This section will showcase images and achievements from the training sessions. Stay tuned for updates!</div>', unsafe_allow_html=True)
