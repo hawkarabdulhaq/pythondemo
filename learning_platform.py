50e6def99024f94382a5d8aba56e295bf912682a
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-02 10:30:33 +0100

    Create learning_platform.py

---
+++ learning_platform.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def show():
+    st.markdown('<div class="title">Learning Platform</div>', unsafe_allow_html=True)
+    st.markdown('<div class="content">Our dedicated learning platform will provide resources, assignments, and quizzes. Updates coming soon!</div>', unsafe_allow_html=True)
+
