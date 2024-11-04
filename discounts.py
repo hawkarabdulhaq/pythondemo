74e89c3c820ce6c1f821d33f4091ebc3f42c9f50
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-04 23:42:08 +0100

    Update discounts.py

--- discounts.py
+++ discounts.py
@@ -11,9 +11,9 @@ def show():
         <ul>
             <li><strong>Earn Discounts with Friend Referrals</strong>: Invite a friend, and they receive a <strong>15% discount</strong> on the course fee. You also receive a <strong>15% discount</strong> as a thank-you!</li>
             <li><strong>Refer Three, Get Your Course Free</strong>: Successfully refer three friends who enroll in the course, and your entire course fee will be <strong>completely waived</strong>.</li>
-            <li><strong>Student Discount for 3rd and 4th Graders</strong>: If you're a 3rd or 4th-grade student, you’re eligible for an <strong>80% discount</strong> on the total course price.</li>
+            <li><strong>Student Discount for 3rd and 4th Graders</strong>: If you're a 3rd or 4th-year student, you’re eligible for an <strong>80% discount</strong> on the total course price.</li>
         </ul>
         
-        <p>For more information, reach out to us at <a href="mailto:connect@habdulhaq.com" style="color: #1ABC9C;">connect@habdulhaq.com</a>.</p>
+        <p>For more information, reach out to us at <a href="mailto:connect@habdulhaq.com" target="_blank" style="color: #1ABC9C;">connect@habdulhaq.com</a>.</p>
     </div>
     """, unsafe_allow_html=True)
