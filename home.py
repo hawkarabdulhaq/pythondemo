bfa7906e6e0983fc541c9739fe8d845dc3b4d938
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-07 06:34:30 +0100

    Update home.py

--- home.py
+++ home.py
@@ -8,7 +8,7 @@ def show():
     st.markdown("""
     <div class="video-container">
         <h3>Watch the demo video and get to know about the course</h3>
-        <iframe width="853" height="480" src="https://www.youtube.com/embed/G8BC2NIfpAs" 
+        <iframe width="853" height="600" src="https://www.youtube.com/embed/G8BC2NIfpAs" 
         title="ڕاهێنانی کۆدینگ لەگەڵ هاوکار" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
         encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" 
         allowfullscreen></iframe>
