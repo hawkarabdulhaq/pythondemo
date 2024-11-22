4334830060ae750651593a5bf5846889a585aed4
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-22 20:58:46 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,19 +1,16 @@
 # dictionary.py
 
-# Import translations from various modules
 from translations.app_translation import app_translations
 from translations.home_translation import home_translations
-from translations.fit_translation import fit_translations  # Import fit translations
+from translations.fit_translation import fit_translations
+from translations.learning_platform_translation import learning_platform_translations
 
-# Combine all translations into a single dictionary
 translations = {
     **app_translations,
     **home_translations,
-    **fit_translations,  # Add Fit Assessment translations
-    # Add other translation modules here as needed
+    **fit_translations,
+    **learning_platform_translations,
 }
 
-# Centralized translate function
 def translate(key, language):
-    """Retrieve a translated string for the given key and language."""
     return translations.get(key, {}).get(language, key)
