f9ead5b2c1089f3d5146710736f555e573de60ff
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-22 21:19:48 +0100

    Create discount_translation.py

---
+++ translations/discount_translation.py
@@ -0,0 +1,61 @@
+# translations/discount_translation.py
+
+discount_translations = {
+    # Title
+    "discount_title": {
+        "EN": "Discount Opportunities",
+        "KU": "هەڵبژاردنی داشکاندنەکان",
+    },
+
+    # Description
+    "discount_description": {
+        "EN": "Take advantage of our discount options to make learning more affordable and rewarding!",
+        "KU": "بەشداری لە هەڵبژاردنی داشکاندنەکان بکە تا فێربوون بە ئەرزانتر و گەشەپێدارتر بکەیت!",
+    },
+
+    # Friend Referral Discount
+    "friend_referral_title": {
+        "EN": "Earn Discounts with Friend Referrals",
+        "KU": "داشکاندنەکان بە ناردنی هاوڕێ بەدەست بهێنە",
+    },
+    "friend_referral_description": {
+        "EN": (
+            "Invite a friend, and they receive a **15% discount** on the course fee. "
+            "You also receive a **15% discount** as a thank-you!"
+        ),
+        "KU": (
+            "هاوڕێکت بانگ بکە، و ئەوان **١٥% داشکاندن** لە نرخی کۆرس وەردەگرن. "
+            "تۆش **١٥% داشکاندن** وەردەگریت وەکوو سوپاس بۆ."
+        ),
+    },
+
+    # Refer Three Friends
+    "refer_three_title": {
+        "EN": "Refer Three, Get Your Course Free",
+        "KU": "سێ کەس بنێرە، کۆرست بەخۆڕایی بەدەست بهێنە",
+    },
+    "refer_three_description": {
+        "EN": (
+            "Successfully refer three friends who enroll in the course, and your entire course fee will be **completely waived**."
+        ),
+        "KU": (
+            "ئەگەر سێ هاوڕێی سەرکەوتوو بنێریت کە بەشداربنە لە کۆرسەکە، نرخی تەواوی کۆرسەکەت **بە کامڵیەوە لابردراوە**."
+        ),
+    },
+
+    # Student Discount
+    "student_discount_title": {
+        "EN": "Student Discount for 3rd and 4th-Year Students",
+        "KU": "داشکاندنی خوێندکار بۆ خوێندکارانی ساڵی ٣ و ٤",
+    },
+    "student_discount_description": {
+        "EN": "If you're a 3rd or 4th-year student, you’re eligible for an **80% discount** on the total course price.",
+        "KU": "ئەگەر تۆ خوێندکاری ساڵی ٣ یان ٤ بیت، تۆ چاوپێچ بۆ **٨٠% داشکاندن** لەسەر نرخە گشتی کۆرسەکە.",
+    },
+
+    # Contact Information
+    "discount_contact": {
+        "EN": "For more information, reach out to us at [connect@habdulhaq.com](mailto:connect@habdulhaq.com).",
+        "KU": "بۆ زانیاری زیاتر، پەیوەندی بکە بە [connect@habdulhaq.com](mailto:connect@habdulhaq.com).",
+    },
+}
