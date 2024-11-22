e6211f9d8f647ab2266be30b71b7d444d51bb714
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-22 21:03:30 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -4,12 +4,14 @@ from translations.app_translation import app_translations
 from translations.home_translation import home_translations
 from translations.fit_translation import fit_translations
 from translations.learning_platform_translation import learning_platform_translations
+from translations.enrollment_translation import enrollment_translations  # Add enrollment translations
 
 translations = {
     **app_translations,
     **home_translations,
     **fit_translations,
     **learning_platform_translations,
+    **enrollment_translations,  # Include enrollment translations
 }
 
 def translate(key, language):
