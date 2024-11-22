eac617c942b25eb4184dba95722ad9a9be4982ef
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-22 20:28:22 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -2,9 +2,11 @@
 
 # Import translations from various modules
 from translations.app_translation import app_translations
+from translations.home_translation import home_translations
 
 # Combine all translations into a single dictionary
 translations = {
     **app_translations,
-    # Add other translation modules here, e.g., home_translations, fit_translations
+    **home_translations,
+    # Add other translation modules here as needed
 }
