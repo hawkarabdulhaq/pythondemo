ca183c26d99d8b9bc01520c08dfbfe3929dd1c1a
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-22 21:23:27 +0100

    Create certificate_translation.py

---
+++ translations/certificate_translation.py
@@ -0,0 +1,38 @@
+# translations/certificate_translation.py
+
+certificate_translations = {
+    # Titles and descriptions
+    "certificate_title": {
+        "EN": "Course Completion Certificates",
+        "KU": "بەڵگەنامەکانی تەواوبوونی کۆرس",
+    },
+    "certificate_description": {
+        "EN": (
+            "Trainees will receive a certificate upon successfully completing all course requirements. This certificate is a testament to your "
+            "dedication, hard work, and mastery of Python programming and automation. Below are the two styles of certificates available:"
+        ),
+        "KU": (
+            "خوێندکاران بەڵگەنامەی وەرە دەگرن کاتێک هەموو پێویستەکانی کۆرسەکە بە سەرکەوتوویی تەواوبکەن. "
+            "ئەم بەڵگەنامە بەرزبوونی کەوشا، هەوڵ، و توانایان لە کۆدینگی پایتۆن و خۆکارکردن دیار دەکات. "
+            "خوارەوە دوو شێوەی بەڵگەنامە بەردەستە:"
+        ),
+    },
+    
+    # Certificate types
+    "portrait_certificate_title": {
+        "EN": "Portrait Certificate",
+        "KU": "بەڵگەنامەی پەیژە",
+    },
+    "portrait_certificate_alt": {
+        "EN": "Portrait Certificate",
+        "KU": "بەڵگەنامەی پەیژە",
+    },
+    "landscape_certificate_title": {
+        "EN": "Landscape Certificate",
+        "KU": "بەڵگەنامەی ڕووبەر",
+    },
+    "landscape_certificate_alt": {
+        "EN": "Landscape Certificate",
+        "KU": "بەڵگەنامەی ڕووبەر",
+    },
+}
