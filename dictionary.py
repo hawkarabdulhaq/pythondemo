34b341552ed7baca2e7497ea46e0da9d030f1bed
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-22 20:05:29 +0100

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,56 @@
+# dictionary.py
+
+translations = {
+    "home_title": {
+        "EN": "Home",
+        "KU": "ماڵەوە",
+    },
+    "fit_assessment_title": {
+        "EN": "Fit Assessment",
+        "KU": "تاقیکردنەوەی گەواستنەوە",
+    },
+    "learning_title": {
+        "EN": "Learning",
+        "KU": "فێربوون",
+    },
+    "certificate_title": {
+        "EN": "Certificate",
+        "KU": "بەڵگەنامە",
+    },
+    "prices_title": {
+        "EN": "Prices",
+        "KU": "نرخەکان",
+    },
+    "about_title": {
+        "EN": "About",
+        "KU": "دەربارەی",
+    },
+    "contact": {
+        "EN": "Contact",
+        "KU": "پەیوەندی",
+    },
+    "email": {
+        "EN": "Email",
+        "KU": "ئیمەیڵ",
+    },
+    "website": {
+        "EN": "Website",
+        "KU": "ماڵپەڕ",
+    },
+    "discord": {
+        "EN": "Discord Server",
+        "KU": "ڕاژەی دیسکۆرد",
+    },
+    "book_demo": {
+        "EN": "Book a Free Demo Session",
+        "KU": "کاتێکی ڕەگەزکردن بۆ ئەمشانە",
+    },
+    "schedule_demo": {
+        "EN": "Schedule a Demo on Calendly",
+        "KU": "هەڵبژاردنی دیمۆ لە Calendly",
+    },
+    "footer": {
+        "EN": "All rights reserved to",
+        "KU": "هەموو مافەکان پارێزراون بۆ",
+    },
+}
