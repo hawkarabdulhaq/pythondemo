bc7d32c26b359c2673b72f5134d1c471986fe20d
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-11-22 21:06:20 +0100

    Update price.py

--- price.py
+++ price.py
@@ -1,8 +1,10 @@
-# price.py
 import streamlit as st
+from dictionary import translate  # Import the centralized translate function
 
 def show():
-    st.markdown('<div class="title">Course Pricing Options</div>', unsafe_allow_html=True)
+    language = st.session_state.language  # Retrieve the selected language
+
+    st.markdown(f'<div class="title">{translate("course_pricing_title", language)}</div>', unsafe_allow_html=True)
     
     # Define columns for the four pricing options
     col1, col2, col3, col4 = st.columns(4)
@@ -15,15 +17,15 @@ def show():
     with col1:
         st.markdown(f"""
         <div style="background-color: #f4f8fb; padding: 20px; border-radius: 10px; text-align: center; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);">
-            <h3 style="color: #1ABC9C; font-size: 1.5em;">Student</h3>
-            <h4 style="color: #333; font-size: 1.1em; margin-top: 5px;">39,000 IQD</h4>
-            <div style="{feature_style}">Full access to the platform</div>
-            <div style="{feature_style}">Generalized live coding</div>
-            <div style="{feature_style}">5 interactive group sessions</div>
-            <div style="{feature_style}">Pre-defined final project</div>
-            <div style="{feature_style}">Course certificate</div>
-            <div style="{feature_style}">Code for Impact membership</div>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfolc_O8_QIn25TylAOwTIKOpHpBV3gG_V4bja79B0EopRLVQ/viewform?usp=sf_link" target="_blank" style="{button_style}">Enroll Now</a>
+            <h3 style="color: #1ABC9C; font-size: 1.5em;">{translate("student_offer_title", language)}</h3>
+            <h4 style="color: #333; font-size: 1.1em; margin-top: 5px;">{translate("student_offer_price", language)}</h4>
+            <div style="{feature_style}">{translate("student_feature_1", language)}</div>
+            <div style="{feature_style}">{translate("student_feature_2", language)}</div>
+            <div style="{feature_style}">{translate("student_feature_3", language)}</div>
+            <div style="{feature_style}">{translate("student_feature_4", language)}</div>
+            <div style="{feature_style}">{translate("student_feature_5", language)}</div>
+            <div style="{feature_style}">{translate("student_feature_6", language)}</div>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfolc_O8_QIn25TylAOwTIKOpHpBV3gG_V4bja79B0EopRLVQ/viewform?usp=sf_link" target="_blank" style="{button_style}">{translate("enroll_button", language)}</a>
         </div>
         """, unsafe_allow_html=True)
 
@@ -31,15 +33,15 @@ def show():
     with col2:
         st.markdown(f"""
         <div style="background-color: #f4f8fb; padding: 20px; border-radius: 10px; text-align: center; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);">
-            <h3 style="color: #1ABC9C; font-size: 1.5em;">Group</h3>
-            <h4 style="color: #333; font-size: 1.1em; margin-top: 5px;">195,000 IQD per person</h4>
-            <div style="{feature_style}">Full access to the platform</div>
-            <div style="{feature_style}">Generalized live coding</div>
-            <div style="{feature_style}">5 group sessions</div>
-            <div style="{feature_style}">Pre-defined final project</div>
-            <div style="{feature_style}">Course certificate</div>
-            <div style="{feature_style}">Code for Impact membership</div>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfolc_O8_QIn25TylAOwTIKOpHpBV3gG_V4bja79B0EopRLVQ/viewform?usp=sf_link" target="_blank" style="{button_style}">Enroll Now</a>
+            <h3 style="color: #1ABC9C; font-size: 1.5em;">{translate("group_offer_title", language)}</h3>
+            <h4 style="color: #333; font-size: 1.1em; margin-top: 5px;">{translate("group_offer_price", language)}</h4>
+            <div style="{feature_style}">{translate("group_feature_1", language)}</div>
+            <div style="{feature_style}">{translate("group_feature_2", language)}</div>
+            <div style="{feature_style}">{translate("group_feature_3", language)}</div>
+            <div style="{feature_style}">{translate("group_feature_4", language)}</div>
+            <div style="{feature_style}">{translate("group_feature_5", language)}</div>
+            <div style="{feature_style}">{translate("group_feature_6", language)}</div>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfolc_O8_QIn25TylAOwTIKOpHpBV3gG_V4bja79B0EopRLVQ/viewform?usp=sf_link" target="_blank" style="{button_style}">{translate("enroll_button", language)}</a>
         </div>
         """, unsafe_allow_html=True)
 
@@ -47,17 +49,16 @@ def show():
     with col3:
         st.markdown(f"""
         <div style="background-color: #f4f8fb; padding: 20px; border-radius: 10px; text-align: center; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);">
-            <h3 style="color: #1ABC9C; font-size: 1.5em;">One-on-One</h3>
-            <h4 style="color: #333; font-size: 1.1em; margin-top: 5px;">395,000 IQD</h4>
-            <div style="{feature_style}">Full access to the platform</div>
-            <div style="{feature_style}">5 one-on-one sessions</div>
-            <div style="{feature_style}">Personalized live coding</div>
-            <div style="{feature_style}">Custom final project</div>
-            <div style="{feature_style}">Course certificate</div>
-            <div style="{feature_style}">LinkedIn reference</div>
-            <div style="{feature_style}">Follow-up support</div>
-            <div style="{feature_style}">Code for Impact membership</div>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfolc_O8_QIn25TylAOwTIKOpHpBV3gG_V4bja79B0EopRLVQ/viewform?usp=sf_link" target="_blank" style="{button_style}">Enroll Now</a>
+            <h3 style="color: #1ABC9C; font-size: 1.5em;">{translate("one_on_one_offer_title", language)}</h3>
+            <h4 style="color: #333; font-size: 1.1em; margin-top: 5px;">{translate("one_on_one_offer_price", language)}</h4>
+            <div style="{feature_style}">{translate("one_on_one_feature_1", language)}</div>
+            <div style="{feature_style}">{translate("one_on_one_feature_2", language)}</div>
+            <div style="{feature_style}">{translate("one_on_one_feature_3", language)}</div>
+            <div style="{feature_style}">{translate("one_on_one_feature_4", language)}</div>
+            <div style="{feature_style}">{translate("one_on_one_feature_5", language)}</div>
+            <div style="{feature_style}">{translate("one_on_one_feature_6", language)}</div>
+            <div style="{feature_style}">{translate("one_on_one_feature_7", language)}</div>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfolc_O8_QIn25TylAOwTIKOpHpBV3gG_V4bja79B0EopRLVQ/viewform?usp=sf_link" target="_blank" style="{button_style}">{translate("enroll_button", language)}</a>
         </div>
         """, unsafe_allow_html=True)
 
@@ -65,17 +66,15 @@ def show():
     with col4:
         st.markdown(f"""
         <div style="background-color: #f4f8fb; padding: 20px; border-radius: 10px; text-align: center; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);">
-            <h3 style="color: #1ABC9C; font-size: 1.5em;">Enterprise</h3>
-            <h4 style="color: #333; font-size: 1.1em; margin-top: 5px;">950,000 IQD <br> <span style="font-size: 0.9em;">(3-5 participants)</span></h4>
-            <div style="{feature_style}">Exclusive one-on-one sessions</div>
-            <div style="{feature_style}">Full access to the platform</div>
-            <div style="{feature_style}">5 personalized sessions per participant</div>
-            <div style="{feature_style}">Custom live coding guidance</div>
-            <div style="{feature_style}">Custom final project</div>
-            <div style="{feature_style}">Course certificate</div>
-            <div style="{feature_style}">LinkedIn reference</div>
-            <div style="{feature_style}">Ongoing follow-up support</div>
-            <div style="{feature_style}">Code for Impact membership</div>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfolc_O8_QIn25TylAOwTIKOpHpBV3gG_V4bja79B0EopRLVQ/viewform?usp=sf_link" target="_blank" style="{button_style}">Enroll Now</a>
+            <h3 style="color: #1ABC9C; font-size: 1.5em;">{translate("enterprise_offer_title", language)}</h3>
+            <h4 style="color: #333; font-size: 1.1em; margin-top: 5px;">{translate("enterprise_offer_price", language)}</h4>
+            <div style="{feature_style}">{translate("enterprise_feature_1", language)}</div>
+            <div style="{feature_style}">{translate("enterprise_feature_2", language)}</div>
+            <div style="{feature_style}">{translate("enterprise_feature_3", language)}</div>
+            <div style="{feature_style}">{translate("enterprise_feature_4", language)}</div>
+            <div style="{feature_style}">{translate("enterprise_feature_5", language)}</div>
+            <div style="{feature_style}">{translate("enterprise_feature_6", language)}</div>
+            <div style="{feature_style}">{translate("enterprise_feature_7", language)}</div>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfolc_O8_QIn25TylAOwTIKOpHpBV3gG_V4bja79B0EopRLVQ/viewform?usp=sf_link" target="_blank" style="{button_style}">{translate("enroll_button", language)}</a>
         </div>
         """, unsafe_allow_html=True)
