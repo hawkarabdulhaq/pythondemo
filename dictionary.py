adf90d6de0cf2b2a98ed832a8d02cf88282e2bdd
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2025-01-19 16:27:09 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -8,6 +8,7 @@ from translations.price_translation import price_translations
 from translations.discount_translation import discount_translations
 from translations.certificate_translation import certificate_translations
 from translations.about_translation import about_translations  # Import about translations
+from translations.trainings_translation import trainings_translations  # Import trainings translations
 
 # Combine all translations into a single dictionary
 translations = {
@@ -20,6 +21,7 @@ translations = {
     **discount_translations,
     **certificate_translations,
     **about_translations,
+    **trainings_translations,  # Add trainings translations
 }
 
 # Centralized translate function
