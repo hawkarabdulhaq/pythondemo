17e2f1c86829a3ad8ba0776d1b4eca8fcf9771b9
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2025-01-28 18:29:25 +0100

    Create ourtrainings.py

---
+++ ourtrainings.py
@@ -0,0 +1,12 @@
+# ourtrainings.py
+
+def get_trainings():
+    """
+    Returns a list of available trainings.
+    """
+    trainings = [
+        "Python Programming through Generative AI for Beginners",
+        "Micro Master Machine Learning and Data-Driven Systems",
+        "Business Optimization through Advanced Automation",
+    ]
+    return trainings
