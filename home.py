d038e0342ec8a8cc5375619477df07325008fcf9
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2025-02-16 08:43:29 +0100

    Update home.py

--- home.py
+++ home.py
@@ -40,7 +40,7 @@ def show():
         <div style="display: flex; justify-content: space-around; flex-wrap: wrap;">
             <div style="background-color: #000000; padding: 20px; border-radius: 10px; width: 30%; margin-bottom: 20px; 
                         box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); text-align: center;">
-                <img src="https://raw.githubusercontent.com/hawkarabdulhaq/pythondemo/main/input/training.jpg" alt="Training" 
+                <img src="https://raw.githubusercontent.com/hawkarabdulhaq/pythondemo/main/input/training.jpg" alt="Steps-to-Expert" 
                      style="border-radius: 10px; margin-bottom: 15px; width: 100%; height: auto;">
                 <h3 style="color: #eeeeee;">Training</h3>
                 <p style="color: #eeeeee;">Learn AI, Machine Learning, and Automation quickly and effectively.</p>
